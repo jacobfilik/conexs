664399bfd95628324923425cea9cfd1ed10b8407
adaudon <odm32222@ws398.diamond.ac.uk>
2024-09-10 19:20:28 +0100

    Added 3d molecule viewer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
+import { useEffect } from 'react';
 import './App.css'
 
 function App() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    
+    script.src = "https://3Dmol.org/build/3Dmol.js";
+    script.async = true;
+  
+    document.body.appendChild(script);
+  
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, []);
 
   return (
     <>
-      <p>Hello Diamond!</p>
+        <h1>3D Molecule Viewer</h1>
+        <div
+            style={{height: "400px", width: "400px", position: 'relative'}}
+            className='viewer_3Dmoljs'
+            data-type="xyz"
+            data-href="./src/assets/glucose.xyz"
+            data-backgroundcolor='cyan'
+            data-style='stick'></div>
     </>
   )
 }
