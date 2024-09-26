9d1ea6fdd74b167da10e837520adbbcdbb9b47d8
adaudon <odm32222@ws398.diamond.ac.uk>
2024-09-26 13:52:44 +0100

    Section/Full mode design implemented

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,26 @@ import { useState } from 'react'
 import './App.css'
 import Molecule3D from './Components/Molecule3D'
 import MoleculeDataTextArea from './Components/moleculeDataTextArea'
-import { Box, Grid2, TextField, Tooltip, Typography } from '@mui/material'
+import { Box, Grid2, Tooltip, Typography } from '@mui/material'
 import StyleToggle from './Components/StyleToggle'
+import FormatAlignJustifyIcon from '@mui/icons-material/FormatAlignJustify';
+import MoleculeDataTextAreaFull from './Components/moleculeDataTextAreaFull'
 
 function App() {
 
   const [color, setcolor] = useState("#3465A4")
   const [moleculedata, setmoleculeData] = useState('5\nLattice="2.8910999499435217 0.0 0.0 -1.4455499749717609 2.503766001531009 0.0 0.0 0.0 12.0" size="1 1" sym_prec=0.3\nV        0.163920355   2.010556463  5.207822443\nV        1.659549808   1.146964790  2.792551218\nC        0.186758046   0.323055004  4.000202257\nO        1.641249387   1.157827410  6.208219945\nO        0.182400454   2.000036854  1.792200686')
   const [style, setStyle] = useState("Stick");
+  const [isFull, setFull] = useState(true)
+
+  const a = moleculedata.split("\n");
+  const sectionedmoleculedata = {
+    atoms: a[0],
+    comments: a[1],
+    data: a.slice(2)
+
+  }
+  console.log(sectionedmoleculedata);
 
   return (
     // <Container sx={{bgcolor: "#597991"}}>
@@ -26,28 +38,17 @@ function App() {
           </Box>
         </Grid2>
         <Grid2 size={5}>
-          <TextField
-            id="atomBox"
-            label="Atoms"
-            defaultValue={1}
-            sx={{m: 2, width: "64px"}}
-            disabled
-          />
-          <TextField
-            id="atomBox"
-            label="Atoms"
-            defaultValue={1}
-            sx={{m: 2, width: "calc(100% - 128px)"}}
-          />
-          <MoleculeDataTextArea moleculedata={moleculedata} setmoleculeData={setmoleculeData} />
+          <div style={{position: "relative"}}>
+            {isFull ? <MoleculeDataTextAreaFull moleculedata={moleculedata} setmoleculeData={setmoleculeData} /> : <MoleculeDataTextArea moleculedata={sectionedmoleculedata} setmoleculeData={setmoleculeData} />}
+            <div onClick={() => setFull(!isFull)} ><FormatAlignJustifyIcon sx={{color: {color}, position: "absolute", top: 115, right: -5}} /></div>
+          </div>
         </Grid2>
       </Grid2>
-        
-        {/* Count lines instead
-        atom line format is symbol then three numbers
-        Validation
-        Make it look nice with MUI
-        */}
+      {/* Count lines instead
+      atom line format is symbol then three numbers
+      Validation
+      Make it look nice with MUI
+      */}
     </>
     // </Container>
   )
--- src/Components/moleculeDataTextArea.tsx
+++ src/Components/moleculeDataTextArea.tsx
@@ -1,22 +1,37 @@
 import { TextField } from "@mui/material";
 
 type Molecule3DProps = {
-    moleculedata: string,
+    moleculedata: object,
     setmoleculeData: (moleculedata: string)=> void
 }
 
 function MoleculeDataTextArea(props: Molecule3DProps) {
       
     return (
+        <>
+        <TextField
+            id="atomBox"
+            label="Atoms"
+            defaultValue={props.moleculedata["atoms"]}
+            sx={{m: 2, width: "64px"}}
+            disabled
+        />
+        <TextField
+            id="Comment/Lattice"
+            label="Comment/Lattice"
+            defaultValue={props.moleculedata["comments"]}
+            sx={{m: 2, width: "calc(100% - 128px)"}}
+        />
         <TextField
         id="datafilebox"
         label="Molecule Data"
         multiline
         rows={30}
-        defaultValue={props.moleculedata}
+        defaultValue={props.moleculedata["data"]}
         sx={{m: 2, width: "100%"}}
         onChange={(e) => props.setmoleculeData(e.target.value)} style={{resize: "none"}}
       />
+      </>
 )}
 
 export default MoleculeDataTextArea;
