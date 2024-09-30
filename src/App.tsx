454cd75a40ffb3e276cf18b3cbe914b1f378d706
adaudon <odm32222@ws398.diamond.ac.uk>
2024-09-30 16:55:52 +0100

    Changed rendering from live to on button press for performance reasons. Also added validation and started an attempt at design.

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,17 @@ import Molecule3D from './Components/Molecule3D'
 import MoleculeDataTextArea from './Components/moleculeDataTextArea'
 import { Box, Grid2, Tooltip, Typography } from '@mui/material'
 import StyleToggle from './Components/StyleToggle'
-import FormatAlignJustifyIcon from '@mui/icons-material/FormatAlignJustify';
-import MoleculeDataTextAreaFull from './Components/moleculeDataTextAreaFull'
 
 function App() {
 
   const [color, setcolor] = useState("#3465A4")
-  const [moleculedata, setmoleculeData] = useState('5\nLattice="2.8910999499435217 0.0 0.0 -1.4455499749717609 2.503766001531009 0.0 0.0 0.0 12.0" size="1 1" sym_prec=0.3\nV        0.163920355   2.010556463  5.207822443\nV        1.659549808   1.146964790  2.792551218\nC        0.186758046   0.323055004  4.000202257\nO        1.641249387   1.157827410  6.208219945\nO        0.182400454   2.000036854  1.792200686')
   const [style, setStyle] = useState("Stick");
-  const [isFull, setFull] = useState(true)
-
-  const a = moleculedata.split("\n");
-  const sectionedmoleculedata = {
-    atoms: a[0],
-    comments: a[1],
-    data: a.slice(2)
-
-  }
-  console.log(sectionedmoleculedata);
+  const [moleculedata, setmoleculeData] = useState('5\nLattice="2.8910999499435217 0.0 0.0 -1.4455499749717609 2.503766001531009 0.0 0.0 0.0 12.0" size="1 1" sym_prec=0.3\nV        0.163920355   2.010556463  5.207822443\nV        1.659549808   1.146964790  2.792551218\nC        0.186758046   0.323055004  4.000202257\nO        1.641249387   1.157827410  6.208219945\nO        0.182400454   2.000036854  1.792200686')
 
   return (
-    // <Container sx={{bgcolor: "#597991"}}>
     <>
-      <Typography variant='h1' sx={{color: "black", textAlign: "center"}}>3D Molecule Viewer</Typography>
-      <Grid2 container spacing={2} sx={{bgcolor: "#597991"}}>
+      <Typography variant='h1' sx={{textAlign: "center"}}>3D Molecule Viewer</Typography>
+      <Grid2 container spacing={2} sx={{}}>
         <Grid2 size={6} sx={{display: "grid", gridAutoRows: "80% 20%", margin: 2, gap: 3}}>
           <Molecule3D key={color} color={color} moleculedata={moleculedata} style={style} />
           <Box sx={{textAlign: "center"}}>
@@ -39,18 +26,11 @@ function App() {
         </Grid2>
         <Grid2 size={5}>
           <div style={{position: "relative"}}>
-            {isFull ? <MoleculeDataTextAreaFull moleculedata={moleculedata} setmoleculeData={setmoleculeData} /> : <MoleculeDataTextArea moleculedata={sectionedmoleculedata} setmoleculeData={setmoleculeData} />}
-            <div onClick={() => setFull(!isFull)} ><FormatAlignJustifyIcon sx={{color: {color}, position: "absolute", top: 115, right: -5}} /></div>
+            <MoleculeDataTextArea moleculedata={moleculedata} setmoleculeData={setmoleculeData} />
           </div>
         </Grid2>
       </Grid2>
-      {/* Count lines instead
-      atom line format is symbol then three numbers
-      Validation
-      Make it look nice with MUI
-      */}
     </>
-    // </Container>
   )
 }
 
--- src/Components/moleculeDataTextArea.tsx
+++ src/Components/moleculeDataTextArea.tsx
@@ -1,36 +1,82 @@
-import { TextField } from "@mui/material";
+import { Alert, Button, TextField } from "@mui/material";
+import { useState } from "react";
 
 type Molecule3DProps = {
-    moleculedata: object,
+    moleculedata: string,
     setmoleculeData: (moleculedata: string)=> void
 }
 
 function MoleculeDataTextArea(props: Molecule3DProps) {
-      
+    const [atoms, setAtoms] = useState(0)
+    const [comment, setComment] = useState("")
+    const [data, setData] = useState("")
+    const [error, setError] = useState([""])
+    const [isError, setIsError] = useState(false)
+    
+    function renderMolecule() {
+        const a = data.split("\n")
+        let errorList = ""
+        for (let index = 0; index < a.length; index++) {
+            const b = a[index].split(/\b\s+/)
+            if (b.length!=4) {
+                errorList = errorList + "Wrong number of items on line " + (index + 1) + "\n"
+            }
+            if (!/^[a-zA-Z]+$/.test(b[0])) {
+                errorList = errorList + "Invalid chemical on line " + (index + 1) + "\n"
+            }
+            if (!/^[+-]?[0-9]{1,}(?:\.[0-9]{1,})?$/.test(b[1]) || !/^[+-]?[0-9]{1,}(?:\.[0-9]{1,})?$/.test(b[2]) || !/^[+-]?[0-9]{1,}(?:\.[0-9]{1,})?$/.test(b[3])) {
+                errorList = errorList + "Invalid number on line " + (index + 1) + "\n"
+                setIsError(true)
+            }
+        }
+        if (errorList == "") {
+            setIsError(false)
+            props.setmoleculeData(atoms + "\n" + comment + "\n" + data)
+        } else {
+            const temp = errorList.split("\n");
+            temp.length = temp.length -1
+            setError(temp)}
+    }
+
     return (
         <>
         <TextField
             id="atomBox"
             label="Atoms"
-            defaultValue={props.moleculedata["atoms"]}
+            value={atoms}
             sx={{m: 2, width: "64px"}}
             disabled
         />
         <TextField
             id="Comment/Lattice"
             label="Comment/Lattice"
-            defaultValue={props.moleculedata["comments"]}
             sx={{m: 2, width: "calc(100% - 128px)"}}
+            onChange={(e) => setComment(e.target.value)}
         />
         <TextField
-        id="datafilebox"
-        label="Molecule Data"
-        multiline
-        rows={30}
-        defaultValue={props.moleculedata["data"]}
-        sx={{m: 2, width: "100%"}}
-        onChange={(e) => props.setmoleculeData(e.target.value)} style={{resize: "none"}}
-      />
+            id="datafilebox"
+            label="Molecule Data"
+            multiline
+            rows={20}
+            sx={{m: 2, width: "100%"}}
+            onChange={(e) => {
+                setAtoms(e.target.value.split("\n").length)
+                setData(e.target.value)
+            }}
+        />
+        <Button
+            variant="contained"
+            sx={{m: 2, width: "100%"}}
+            onClick={renderMolecule}
+        >Render</Button>
+        {isError ? <Alert
+            variant="filled"
+            sx={{m: 2, width: "100%"}}
+            severity="error">
+                <ul style={{padding: 0}}>
+                    {error.map(data => {return <li style={{listStyle: "none"}}>{data}</li>})}
+                </ul>
+        </Alert> : <></>}
       </>
 )}
 
