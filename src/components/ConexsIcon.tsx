e59d45f77c69cebcfc433ac39f855b13025d70be
Jacob Filik <jacob.filik@diamond.ac.uk>
2024-11-29 09:55:24 +0000

    fix svg

--- src/components/ConexsIcon.tsx
+++ src/components/ConexsIcon.tsx
@@ -4,12 +4,7 @@ export default function ConexsIcon() {
   return (
     <SvgIcon fontSize="large">
       <svg
-        xmlns:cc="http://creativecommons.org/ns#"
-        xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-        xmlns:svg="http://www.w3.org/2000/svg"
         xmlns="http://www.w3.org/2000/svg"
-        xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
-        xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
         width="128mm"
         height="128mm"
         stroke="currentColor"
@@ -19,8 +14,6 @@ export default function ConexsIcon() {
         viewBox="0 0 128 128"
         version="1.1"
         id="svg8"
-        inkscape:version="1.0.2 (e86c870879, 2021-01-15)"
-        sodipodi:docname="xas.svg"
       >
         <g>
           <circle id="path10" cx="45" cy="45" r="40" strokeOpacity={0.25} />
