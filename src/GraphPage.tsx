89550bff034ed66b8762deb0efbd4859835f85e1
Jacob Filik <jacob.filik@diamond.ac.uk>
2025-01-07 10:15:35 +0000

    improve layout of result view

--- src/GraphPage.tsx
+++ src/GraphPage.tsx
@@ -7,26 +7,26 @@ import {
   DefaultInteractions,
   ResetZoomButton,
   TooltipMesh,
+  getCombinedDomain,
 } from "@h5web/lib";
 import React, { useState } from "react";
-import { Box, Button, Stack, Typography } from "@mui/material";
+import { Box, Button, Stack, Typography, Grid2 } from "@mui/material";
 
 import { ReactElement } from "react";
 import axios from "axios";
 
-
-const basename = import.meta.env.BASE_URL 
+const basename = import.meta.env.BASE_URL;
 
 export default function GraphPage() {
   const [xdomain, setxDomain] = useState<Domain>([0, 1]);
   const [ydomain, setyDomain] = useState<Domain>([0, 1]);
-  const [xValues, setXValues] = useState<number[]>([0]);
-  const [yValues, setYValues] = useState<number[]>([0]);
-  const [xValues2, setXValues2] = useState<number[]>([0]);
-  const [yValues2, setYValues2] = useState<number[]>([0]);
+  const [xValues, setXValues] = useState<number[] | null>(null);
+  const [yValues, setYValues] = useState<number[] | null>(null);
+  const [xValues2, setXValues2] = useState<number[] | null>(null);
+  const [yValues2, setYValues2] = useState<number[] | null>(null);
   const [showExample, setShowExample] = useState<boolean>(true);
 
-  function twoColumnToTwoArray(content : string) {
+  function twoColumnToTwoArray(content: string) {
     const lines = content.split("\n");
     const xVals: number[] = [];
     const yVals: number[] = [];
@@ -39,7 +39,7 @@ export default function GraphPage() {
       }
     });
 
-    return {x: xVals, y: yVals}
+    return { x: xVals, y: yVals };
   }
 
   function stkFilePreprocessing(content: string) {
@@ -66,7 +66,7 @@ export default function GraphPage() {
     return newContent;
   }
 
-  const handleFileUpload1 = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (!event.target.files) return;
     const file = event.target.files[0];
 
@@ -74,8 +74,16 @@ export default function GraphPage() {
       const reader = new FileReader();
       reader.onload = function () {
         let content = reader.result as string;
+        let setX = setXValues;
+        let setY = setYValues;
+        let altXVals = xValues2;
+        let altYVals = yValues2;
         if (file.type == "application/hyperstudio") {
           content = stkFilePreprocessing(content);
+          setX = setXValues2;
+          setY = setYValues2;
+          altXVals = xValues;
+          altYVals = yValues;
         }
 
         const lines = content.split("\n");
@@ -90,47 +98,16 @@ export default function GraphPage() {
           }
         });
 
-        setXValues(xVals);
-        setYValues(yVals);
-        const xdom = getDomain(xVals);
-        const ydom = getDomain(yVals);
-        setxDomain(xdom ? xdom : xdomain);
-        setyDomain(ydom ? ydom : ydomain);
-      };
+        setX(xVals);
+        setY(yVals);
+        let xdom = getDomain(xVals);
+        let ydom = getDomain(yVals);
 
-      reader.readAsText(file); // Read the file as text
-    }
-  };
-
-  const handleFileUpload2 = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (!event.target.files) return;
-
-    const file = event.target.files[0];
-
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = function () {
-        let content = reader.result as string;
-        if (file.type == "application/hyperstudio") {
-          content = stkFilePreprocessing(content);
+        if (altXVals != null && altYVals != null && xdom && ydom) {
+          xdom = getCombinedDomain([xdom, getDomain(altXVals)]);
+          ydom = getCombinedDomain([ydom, getDomain(altYVals)]);
         }
 
-        const lines = content.split("\n");
-        const xVals: number[] = [];
-        const yVals: number[] = [];
-
-        lines.forEach((line) => {
-          const [x, y] = line.trim().split(/\s+/); // Split by space or tabs
-          if (x && y) {
-            xVals.push(parseFloat(x)); // Parse x values
-            yVals.push(parseFloat(y)); // Parse y values
-          }
-        });
-
-        setXValues2(xVals);
-        setYValues2(yVals);
-        const xdom = getDomain(xVals);
-        const ydom = getDomain(yVals);
         setxDomain(xdom ? xdom : xdomain);
         setyDomain(ydom ? ydom : ydomain);
       };
@@ -148,79 +125,102 @@ export default function GraphPage() {
   };
 
   const fetchExampleData = () => {
-    axios.all([
-    axios.get(basename + "data/orca_result.txt.xes.dat",{headers: {'Content-Type': 'application/plain'}}), axios.get(basename + "data/orca_result.txt.xes.stk", {headers: {'Content-Type': 'application/plain'}})]).then(axios.spread((data1, data2) => {
+    axios
+      .all([
+        axios.get(basename + "data/orca_result.txt.xes.dat", {
+          headers: { "Content-Type": "application/plain" },
+        }),
+        axios.get(basename + "data/orca_result.txt.xes.stk", {
+          headers: { "Content-Type": "application/plain" },
+        }),
+      ])
+      .then(
+        axios.spread((data1, data2) => {
+          const stk = stkFilePreprocessing(data2.data);
+          const dat = data1.data;
 
-    const stk = stkFilePreprocessing(data2.data);
-    const dat = data1.data;
+          const stk_str = stk.toString();
+          const dat_str = dat.toString();
 
-    const stk_str = stk.toString();
-    const dat_str = dat.toString();
+          const stk_data = twoColumnToTwoArray(stk_str);
+          const dat_data = twoColumnToTwoArray(dat_str);
 
-    const stk_data = twoColumnToTwoArray(stk_str)
-    const dat_data = twoColumnToTwoArray(dat_str)
-
-    const xdom = getDomain(dat_data.x);
-    const ydom = getDomain(dat_data.y);
-    setxDomain(xdom ? xdom : xdomain);
-    setyDomain(ydom ? ydom : ydomain);
-    setXValues(stk_data.x);
-    setYValues(stk_data.y);
-    setXValues2(dat_data.x);
-    setYValues2(dat_data.y);
-    setShowExample(false)
-
-    }));
-
-  }
+          const xdom = getDomain(dat_data.x);
+          const ydom = getDomain(dat_data.y);
+          setxDomain(xdom ? xdom : xdomain);
+          setyDomain(ydom ? ydom : ydomain);
+          setXValues(stk_data.x);
+          setYValues(stk_data.y);
+          setXValues2(dat_data.x);
+          setYValues2(dat_data.y);
+          setShowExample(false);
+        })
+      );
+  };
 
   return (
-    <Stack direction="row" height="100%">
-      <Stack>
-        <Typography variant="h3" sx={{ textAlign: "Left" }}>
-          ORCA Result Viewer
-        </Typography>
+    <Grid2 height="100%" container>
+      <Grid2 item padding={1}>
+        <Stack height="100%">
+          <Typography variant="h5" sx={{ textAlign: "center" }}>
+            Upload `.stk` or `.dat` file to visualize
+          </Typography>
+          <Box sx={{ textAlign: "center" }}>
+            <input type="file" onChange={handleFileUpload} />
+            <input type="file" onChange={handleFileUpload} />
+          </Box>
 
-        <Typography variant="h5" sx={{ textAlign: "center" }}>
-          Upload `.stk` or `.dat` file to visualize
-        </Typography>
-        <Box sx={{ textAlign: "center" }}>
-          <input type="file" onChange={handleFileUpload1} />
-          <input type="file" onChange={handleFileUpload2} />
-        </Box>
-
-        {showExample &&
-
-        <Button variant="outlined" sx={{ m: 5 }} onClick={fetchExampleData}>
-          Example of Graph
-        </Button>}
-      </Stack>
-      <VisCanvas
-        abscissaConfig={{
-          showGrid: true,
-          visDomain: [xdomain[0], xdomain[1]],
-        }}
-        ordinateConfig={{
-          showGrid: true,
-          visDomain: [ydomain[0], ydomain[1]],
-        }}
-      >
-        <DefaultInteractions />
-        <TooltipMesh renderTooltip={tooltipText} />
-        <ResetZoomButton />
-        <DataCurve
-          abscissas={xValues}
-          color="green"
-          ordinates={yValues}
-          visible
-        />
-        <DataCurve
-          abscissas={xValues2}
-          color="orange"
-          ordinates={yValues2}
-          visible
-        />
-      </VisCanvas>
-    </Stack>
+          {showExample && (
+            <Button variant="outlined" sx={{ m: 5 }} onClick={fetchExampleData}>
+              Example of Graph
+            </Button>
+          )}
+        </Stack>
+      </Grid2>
+      <Grid2 item height="100%" width="100%" padding={1}>
+        {/* <Paper
+          // flexdirection="column"
+          sx={{
+            height: "100%",
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            backgroundColor: (theme: Theme) => theme.palette.background.default,
+            fontFamily: (theme: Theme) => theme.typography.fontFamily,
+          }}
+        > */}
+        <VisCanvas
+          abscissaConfig={{
+            showGrid: true,
+            visDomain: [xdomain[0], xdomain[1]],
+          }}
+          ordinateConfig={{
+            showGrid: true,
+            visDomain: [ydomain[0], ydomain[1]],
+          }}
+        >
+          <DefaultInteractions />
+          <TooltipMesh renderTooltip={tooltipText} />
+          <ResetZoomButton />
+          {xValues != null && yValues != null && (
+            <DataCurve
+              abscissas={xValues}
+              color="green"
+              ordinates={yValues}
+              visible
+            />
+          )}
+          {xValues2 != null && yValues2 != null && (
+            <DataCurve
+              abscissas={xValues2}
+              color="orange"
+              ordinates={yValues2}
+              visible
+            />
+          )}
+        </VisCanvas>
+        {/* </Paper> */}
+      </Grid2>
+    </Grid2>
   );
 }
